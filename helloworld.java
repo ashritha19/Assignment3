663adc802180e11a6f1aaf1697d27518e0a21e4a
ashritha19 <ashrithareddy19@gmail.com>
2017-04-18 19:18:56 -0400

    SpringMVC

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+package helloWorld;  
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.servlet.ModelAndView;  
+
+@Controller  
+public class HelloWorld {  
+    
+@RequestMapping("/hello")  
+    public ModelAndView helloWorld() {  
+        String message = "HELLO,WORLD!";  
+        return new ModelAndView("helloworld", "message", message);  
+    }  
+} 
